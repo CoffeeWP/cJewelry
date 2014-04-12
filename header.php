0e8237b3cc90179c04cdb7a88abc96ce9944ae29
Or Duan <orduani@gmail.com>
2014-04-12 19:36:41 +0300

    added cart to menu

--- header.php
+++ header.php
@@ -30,11 +30,19 @@
 <!-- 		<?php if ( get_header_image() ) : ?>
 			<img src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="">
 		<?php endif; // End header image check. ?> -->
-
 		<h1 class="site-title"><?php bloginfo( 'name' ); ?></h1></a>
 		<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-		<!-- Primary Menu -->
+		<?php global $woocommerce; ?>
+ 
+		<div class="my-cart">
+			<a class="cart-contents" href="<?php echo $woocommerce->cart->get_cart_url(); ?>" title="<?php _e('View your shopping cart', 'woothemes'); ?>">
+			<img src="<?php bloginfo('stylesheet_directory'); ?>/images/cart-icon.png" />
+			<?php echo sprintf(_n('%d item', '%d items', $woocommerce->cart->cart_contents_count, 'woothemes'), $woocommerce->cart->cart_contents_count);?> - 
+			<?php echo $woocommerce->cart->get_cart_total(); ?>
+			</a>
+		</div>
 		<div class="hr"></div>
+		<!-- Primary Menu -->
 		<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
 	</header><!-- #masthead -->
 
